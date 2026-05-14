0cddab04264fa4d744604f618598f4e4d568faf0
esmeralda <esmand96@gmail.com>
2026-05-14 14:53:15 +0200

    add polymorphism to logger

--- src/main/java/se/kth/IV1350/bikerepairshop/logging/FileLogger.java
+++ src/main/java/se/kth/IV1350/bikerepairshop/logging/FileLogger.java
@@ -1,12 +1,11 @@
-package se.kth.IV1350.bikerepairshop.util;
-
+package se.kth.IV1350.bikerepairshop.logging;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class FileLogger {
+public class FileLogger implements Logger <String> {
     private PrintWriter logStream;
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -30,7 +29,8 @@ public class FileLogger {
      *
      * @param message The string that will be printed to the log file.
      */
-    public void log(String message) {
+    @Override
+    public void logg(String message) {
         String timestamp = LocalDateTime.now().format(FORMATTER);
         logStream.println("[" + timestamp + "] " + message);
     }
---
+++ src/main/java/se/kth/IV1350/bikerepairshop/logging/Logger.java
@@ -0,0 +1,5 @@
+package se.kth.IV1350.bikerepairshop.logging;
+
+public interface Logger <T> {
+    void logg (T message);
+}
